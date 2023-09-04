fe21179d132c4b97ce06792c0c395b967d947622
Kundan-sharma <kundan.sharma@indicchain.com>
2023-09-04 10:32:19 +0530

    Ercc721 Token#

---
+++ ERCC721.sol
@@ -0,0 +1,38 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts@4.9.3/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts@4.9.3/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts@4.9.3/access/Ownable.sol";
+
+contract MyToken is ERC721, ERC721URIStorage, Ownable {
+    constructor() ERC721("MyToken", "MTK") {}
+
+    function safeMint(address to, uint256 tokenId, string memory uri) public onlyOwner
+    {
+        _safeMint(to, tokenId);
+        _setTokenURI(tokenId, uri);
+    }
+     
+  
+
+    // The following functions are overrides required by Solidity.
+
+    function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage) {
+        super._burn(tokenId);
+    }
+
+    function tokenURI(uint256 tokenId) public view override(ERC721, ERC721URIStorage) returns (string memory)
+    {
+        return super.tokenURI(tokenId);
+    }
+
+    function supportsInterface(bytes4 interfaceId) public view override(ERC721, ERC721URIStorage) returns (bool)    
+    {
+        return super.supportsInterface(interfaceId);
+    }
+
+ 
+       
+    
+}
